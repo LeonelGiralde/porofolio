354a99e39e15c521f8db289feb16f1d1428d50f8
Leonel <leonelgiralde22@gmail.com>
2024-08-05 15:02:18 -0300

    modificaciones footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { BsLinkedin, BsGithub, BsEnvelope, BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Footer = () => {
-    return (
-        <div name='about' className='w-full bg-[#0a192f] text-gray-300 py-8'>
+  return (
+    <div name='about' className='w-full bg-[#0a192f] text-gray-300 py-8'>
       <div className='flex flex-col justify-center items-center space-y-4'>
         <div className='flex space-x-4'>
-          <a href="https://www.linkedin.com/in/leonel-giralde/" target="_blank" rel="noreferrer">
+          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
             <BsLinkedin size={30} />
           </a>
-          <a href="https://github.com/LeonelGiralde" target="_blank" rel="noreferrer">
+          <a href="https://www.github.com" target="_blank" rel="noreferrer">
             <BsGithub size={30} />
           </a>
-          <a href="leonelgiralde22@gmail.com">
+          <Link to="email" smooth={true} duration={500}>
             <BsEnvelope size={30} />
-          </a>
-          <a href="https://drive.google.com/file/d/1qGHFTR9B-UMUELrGWvjfyJVGlH2D9rlz/view?usp=sharing" target="_blank" rel="noreferrer">
+          </Link>
+          <a href="/path-to-cv" target="_blank" rel="noreferrer">
             <BsFillPersonLinesFill size={30} />
           </a>
         </div>
