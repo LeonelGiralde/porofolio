ed32baaa6f5c199d949601c3a324678c07edc096
Leonel <leonelgiralde22@gmail.com>
2024-08-08 17:36:21 -0300

    correccion de URLs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
     <div name='about' className='w-full bg-[#0a192f] text-gray-300 py-8'>
       <div className='flex flex-col justify-center items-center space-y-4'>
         <div className='flex space-x-4'>
-          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+          <a href="https://www.linkedin.com/in/leonel-giralde-3ba71619b/" target="_blank" rel="noreferrer">
             <BsLinkedin size={30} />
           </a>
-          <a href="https://www.github.com" target="_blank" rel="noreferrer">
+          <a href="https://github.com/LeonelGiralde?tab=repositories" target="_blank" rel="noreferrer">
             <BsGithub size={30} />
           </a>
           <Link to="email" smooth={true} duration={500}>
             <BsEnvelope size={30} />
           </Link>
-          <a href="/path-to-cv" target="_blank" rel="noreferrer">
+          <a href="https://drive.google.com/file/d/1xGV52XpsCprnFjPhU8hdM3uqwuQMcvTe/view?usp=sharing" target="_blank" rel="noreferrer">
             <BsFillPersonLinesFill size={30} />
           </a>
         </div>
